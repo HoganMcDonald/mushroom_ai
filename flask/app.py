8092994da3b2999b475b3b0ba923949a52040570
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-09-19 17:14:44 -0500

    multi image upload working

---
+++ flask/app.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'uploads/')
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist('image-to-test'):
+        print(file)
+        filename = file.filename
+        destination = '/'.join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    return render_template('success.html')
+
+if __name__=='__main__':
+    app.run(port=5000, debug=True)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Mushroom AI
+
+I have been studying deep learning and recently as of writing this received a certification. I learned in my course how 
+to write a deep neural network for binary classification with a single output neuron. In future courses I will learn 
+how to expand on that.
+
+For now, I would like to implement what I have learned in a simple restful api written in python that can be used to 
+identify mushrooms. My hope is that this api could be used in a colleagues mobile application.
+
+## API outline
+
+#### Routes
+
+/train
+- post
+- receives admin key
+- 
