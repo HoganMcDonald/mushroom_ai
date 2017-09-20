f5ae20e84104081276d5653b930283b5d473954f
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-09-20 13:34:39 -0500

    images successfully being compressed upon upload and reformatted to JPEG.

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, request
+from PIL import Image
+from resizeimage import resizeimage
 
 
 app = Flask(__name__)
@@ -21,11 +23,17 @@ def upload():
         os.mkdir(target)
 
     for file in request.files.getlist('image-to-test'):
-        print(file)
         filename = file.filename
         destination = '/'.join([target, filename])
-        print(destination)
         file.save(destination)
+        print(destination)
+        img = Image.open(destination)
+        print(img)
+        img = resizeimage.resize_height(img, 100)
+        img = resizeimage.resize_width(img, 100)
+        print(img.format)
+        img.save(destination, 'JPEG')
+        print(img)
 
     return render_template('success.html')
 
